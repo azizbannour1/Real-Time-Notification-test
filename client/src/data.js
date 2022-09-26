56bb6e536666a0836b6f78d6d5f28f19810e383a
azizbannour1 <aziz.bannour@esprit.tn>
2022-09-26 23:59:37 +0100

    First commit

---
+++ client/src/data.js
@@ -0,0 +1,16 @@
+export const posts = [
+    {
+      id: 1,
+      username:"john",
+      fullname: "John Keller",
+      userImg: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      postImg: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
+    },
+    {
+      id: 2,
+      username:"monica",
+      fullname: "Monica Stan",
+      userImg: "https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+      postImg: "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+  ];
